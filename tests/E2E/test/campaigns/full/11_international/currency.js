f561dd327b21df38f03109e94427cfc4560e3747
sallemiines <ines.sallemi@outlook.com>
2018-03-12 15:32:27 +0100

    Fixing script

--- tests/E2E/test/campaigns/full/11_international/currency.js
+++ tests/E2E/test/campaigns/full/11_international/currency.js
@@ -3,13 +3,13 @@ const {Menu} = require('../../../selectors/BO/menu.js');
 const {Currencies} = require('../../../selectors/BO/international/currencies');
 let promise = Promise.resolve();
 
-scenario('Case N°1 : Add a new currency in BO', client => {
+scenario('Add CRUD for Currencies', client => {
   scenario('Open the browser and connect to the BO', client => {
     test('should open the browser', () => client.open());
     test('should log in successfully in BO', () => client.signInBO(AccessPageBO));
   }, 'common_client');
 
-  scenario('Go to currencies page and add a new currency', client => {
+  scenario('Case N°1 : Add a new currency in BO', client => {
     test('should go to "Currencies" tab', () => {
       return promise
         .then(() => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu))
@@ -24,58 +24,52 @@ scenario('Case N°1 : Add a new currency in BO', client => {
   }, 'common_client');
 
   scenario('Case N°2 : Update the exchange rates', client => {
-    scenario('Go to currencies page and update the exchange rates', client => {
-      test('should go to "Currencies" tab', () => {
-        return promise
-          .then(() => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu))
-          .then(() => client.waitForExistAndClick(Menu.Improve.International.currencies_tab))
-      });
-      test('should search for the created currency by "ISO CODE" and Update the exchange rates', () => {
-        return promise
-          .then(() => client.searchByValue(Currencies.iso_code_input, Currencies.search_button, 'USD'))
-          .then(() => client.waitForExistAndClick(Currencies.exchange_rate_update_button))
-      });
-      test('should check that the success alert message is well displayed ', () => client.checkTextValue(Currencies.green_validation, '×\nThe settings have been successfully updated.'));
-      test('should check that the value is successfully updated', () => client.checkTextValue(Currencies.exchange_rate_value, '2', 'notequal'))
-    }, 'common_client');
+    test('should go to "Currencies" tab', () => {
+      return promise
+        .then(() => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu))
+        .then(() => client.waitForExistAndClick(Menu.Improve.International.currencies_tab))
+    });
+    test('should search for the created currency by "ISO CODE" and Update the exchange rates', () => {
+      return promise
+        .then(() => client.searchByValue(Currencies.iso_code_input, Currencies.search_button, 'USD'))
+        .then(() => client.waitForExistAndClick(Currencies.exchange_rate_update_button))
+    });
+    test('should check that the success alert message is well displayed ', () => client.checkTextValue(Currencies.green_validation, '×\nThe settings have been successfully updated.'));
+    test('should check that the value is successfully updated', () => client.checkTextValue(Currencies.exchange_rate_value, '2', 'notequal'))
   }, 'common_client');
 
   scenario('Case N°3 : Edit the currency', client => {
-    scenario('Go to currencies page and edit currency status', client => {
-      test('should go to "Currencies" tab', () => {
-        return promise
-          .then(() => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu))
-          .then(() => client.waitForExistAndClick(Menu.Improve.International.currencies_tab))
-      });
-      test('should search for the currency and click on "Edit" button', () => {
-        return promise
-          .then(() => client.searchByValue(Currencies.iso_code_input, Currencies.search_button, 'USD'))
-          .then(() => client.waitForExistAndClick(Currencies.edit_button))
+    test('should go to "Currencies" tab', () => {
+      return promise
+        .then(() => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu))
+        .then(() => client.waitForExistAndClick(Menu.Improve.International.currencies_tab))
+    });
+    test('should search for the currency and click on "Edit" button', () => {
+      return promise
+        .then(() => client.searchByValue(Currencies.iso_code_input, Currencies.search_button, 'USD'))
+        .then(() => client.waitForExistAndClick(Currencies.edit_button))
 
-      });
-      test('should change the currency status ', () => client.waitForExistAndClick(Currencies.currency_status));
-      test('should click on "Save" button', () => client.waitForExistAndClick(Currencies.save_button));
-      test('should check that the success alert message is well displayed ', () => client.checkTextValue(Currencies.green_validation, '×\nSuccessful update.'));
-    }, 'common_client');
+    });
+    test('should change the currency status ', () => client.waitForExistAndClick(Currencies.currency_status));
+    test('should click on "Save" button', () => client.waitForExistAndClick(Currencies.save_button));
+    test('should check that the success alert message is well displayed ', () => client.checkTextValue(Currencies.green_validation, '×\nSuccessful update.'));
   }, 'common_client');
 
   scenario('Case N°4 : Delete the currency', () => {
-    scenario('Go to currencies page and update the exchange rates', client => {
-      test('should go to "Currencies" tab', () => {
-        return promise
-          .then(() => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu))
-          .then(() => client.waitForExistAndClick(Menu.Improve.International.currencies_tab))
-      });
-      test('should search for the currency and click on "Delete" button', () => {
-        return promise
-          .then(() => client.searchByValue(Currencies.iso_code_input, Currencies.search_button, 'USD'))
-          .then(() => client.waitForExistAndClick(Currencies.dropdown_button))
-          .then(() => client.waitForExistAndClick(Currencies.delete_button))
-          .then(() => client.alertAccept())
-      });
-      test('should check that the success alert message is well displayed ', () => client.checkTextValue(Currencies.green_validation, '×\nSuccessful deletion.'));
-      test('should logout successfully from the Front Office', () => client.signOutBO(AccessPageBO));
-    }, 'common_client');
+    test('Go to currencies page ', () => {
+      return promise
+        .then(() => client.goToSubtabMenuPage(Menu.Improve.International.international_menu, Menu.Improve.International.localization_submenu))
+        .then(() => client.waitForExistAndClick(Menu.Improve.International.currencies_tab))
+    });
+    test('should search for the currency and click on "Delete" button', () => {
+      return promise
+        .then(() => client.searchByValue(Currencies.iso_code_input, Currencies.search_button, 'USD'))
+        .then(() => client.waitForExistAndClick(Currencies.dropdown_button))
+        .then(() => client.waitForExistAndClick(Currencies.delete_button))
+        .then(() => client.alertAccept())
+    });
+    test('should check that the success alert message is well displayed ', () => client.checkTextValue(Currencies.green_validation, '×\nSuccessful deletion.'));
+    test('should logout successfully from the Front Office', () => client.signOutBO(AccessPageBO));
   }, 'common_client');
 }, 'common_client', true);
 
--- tests/E2E/test/selectors/BO/international/currencies.js
+++ tests/E2E/test/selectors/BO/international/currencies.js
@@ -1,13 +1,13 @@
 module.exports = {
   Currencies: {
     add_new_button: '//*[@id="page-header-desc-currency-new_currency"]/i',
-    currencies_list: '//*[@id="iso_code"]',
-    exchange_rate_input: '//*[@id="conversion_rate"]',
-    currency_status: '//*[@id="currencyStatus"]',
-    save_button: '//*[@id="currency_form_submit_btn"]',
+    currencies_list: '#iso_code',
+    exchange_rate_input:'#conversion_rate',
+    currency_status: '#currencyStatus',
+    save_button: '#currency_form_submit_btn',
     green_validation: '//*[@id="content"]/div[@class="bootstrap"]/div[contains(@class, "success")]',
     iso_code_input: '//*[@id="table-currency"]//th[@class="center"]//input[@name="currencyFilter_iso_code"]',
-    search_button: '//*[@id="submitFilterButtoncurrency"]',
+    search_button: '#submitFilterButtoncurrency',
     exchange_rate_update_button: '//*[@id="currency_form"]/button',
     exchange_rate_value: '//*[@id="table-currency"]/tbody/tr/td[4]',
     edit_button: '//*[@id="table-currency"]//div[@class="btn-group pull-right"]//a[@class="edit btn btn-default"]',
